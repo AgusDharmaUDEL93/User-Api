2abc82bd47b1d4b743fc0b094c8530eda5088aa8
AgusDharmaUDEL93 <agusdharmaudel@gmail.com>
2024-11-27 19:20:03 +0700

    Initial Commit

---
+++ User-Api/User_ApiApp.swift
@@ -0,0 +1,17 @@
+//
+//  User_ApiApp.swift
+//  User-Api
+//
+//  Created by Agus Dharma Kusuma on 27/11/24.
+//
+
+import SwiftUI
+
+@main
+struct User_ApiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
